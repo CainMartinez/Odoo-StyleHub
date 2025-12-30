84d6f1f3bc120086f6adc9ab0ad5388ef9178489
Cain <cainmartinez2@hotmail.com>
2025-12-30 18:36:51 +0100

    first commit

---
+++ addons/style_hub_base/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'StyleHub',
+    'version': '18.0.1.0.0',
+    'summary': 'StyleHub Módulo Base',
+    'description': """
+        Módulo base para la gestión de una peluquería.
+    """,
+    'author': 'Caín Martínez',
+    'website': 'https://cain-dev.es',
+    'license': 'LGPL-3',
+    'category': 'Sales',
+    'depends': ['base'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
